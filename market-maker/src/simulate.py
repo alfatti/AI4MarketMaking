e8b653a6974b042b33605e6d531bc27730c06741
Ahmad Aghapour <107943830+ahmad-aghapour@users.noreply.github.com>
2024-01-18 22:49:09 +0330

    Update simulate.py

--- market-maker/src/simulate.py
+++ market-maker/src/simulate.py
@@ -27,3 +27,23 @@ def simulate_markov_batch(Q, initial_states, times, num_simulations):
             states_at_times[sim, i] = state
 
     return states_at_times
+
+def simulate_markov_batch_1(Q, initial_states, times, num_simulations , delta_t):
+    num_states = len(Q)
+    num_times = len(times)
+    states_at_times = np.zeros((num_simulations, num_times), dtype=int)
+
+    for sim in range(num_simulations):
+        state = initial_states
+        states_at_times[sim, 0] = state
+        q = Q * delta_t
+        I = np.identity(num_states)
+        p = np.zeros((1,num_states))
+        p[0,state] = 1
+        for i in range(1, num_times):
+          p = np.matmul(p,q+I)
+          #print(p)
+          state = np.random.choice(num_states, p=np.squeeze(p))
+          states_at_times[sim, i] = state
+
+    return states_at_times
