071fd3ef1ad948b413b2633d517f97a65832b92f
Ali Fathi <51869119+alfatti@users.noreply.github.com>
2026-08-04 14:35:07 -0700

    Add files via upload

--- options/src/optmm/__init__.py
+++ options/src/optmm/__init__.py
@@ -1,1 +1,2 @@
-
+"""optmm: option market making solvers (BBG 2020 framework)."""
+from . import config as _config  # noqa: F401
---
+++ options/src/optmm/config.py
@@ -0,0 +1,4 @@
+"""Global numerical config. Imported first."""
+import jax
+
+jax.config.update("jax_enable_x64", True)
